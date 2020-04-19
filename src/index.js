566dd476b6d13bf1a1b301eebd9a03006ab221d1
wijo <wijo.dev@gmail.com>
2020-04-19 22:16:21 +0900

    reactstrap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// styles
+import "assets/css/bootstrap.min.css";
+import "assets/css/now-ui-kit.css";
+import "assets/demo/demo.css";
+import "assets/demo/nucleo-icons-page-styles.css";
+
+// pages for this kit
+import Index from "views/Index.js";
+import NucleoIcons from "views/NucleoIcons.js";
+import LoginPage from "views/examples/LoginPage.js";
+import LandingPage from "views/examples/LandingPage.js";
+import ProfilePage from "views/examples/ProfilePage.js";
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Switch>
+      <Route path="/index" render={props => <Index {...props} />} />
+      <Route
+        path="/nucleo-icons"
+        render={props => <NucleoIcons {...props} />}
+      />
+      <Route
+        path="/landing-page"
+        render={props => <LandingPage {...props} />}
+      />
+      <Route
+        path="/profile-page"
+        render={props => <ProfilePage {...props} />}
+      />
+      <Route path="/login-page" render={props => <LoginPage {...props} />} />
+      <Redirect to="/index" />
+      <Redirect from="/" to="/index" />
+    </Switch>
+  </Switch>
+</BrowserRouter>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
